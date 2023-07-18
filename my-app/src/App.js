9f78b37ccd8faeafce2276090c67f62863ef5311
jimin-ni <jimin326ww@gmail.com>
2023-07-19 02:48:40 +0900

    [0719] 리액트 서버에 power bi 테스트 결과 업로드 성공 ㅠㅠ

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { PowerBIEmbed } from 'powerbi-client-react';
 import { models } from 'powerbi-client';
-
+import React from 'react';
 
 function App() {
   return (
@@ -12,15 +12,18 @@ function App() {
         <PowerBIEmbed
           embedConfig = {{
             type: 'report',   // Supported types: report, dashboard, tile, visual, qna, paginated report and create
-            id: '<Report Id>',
-            embedUrl: '<Embed Url>',
-            accessToken: '<Access Token>',
-            tokenType: models.TokenType.Embed, // Use models.TokenType.Aad for SaaS embed
+            id: "99fc4531-73f3-4bda-b8f9-ef2d9948d577",  //* '<Report Id>',
+            
+            embedUrl: "https://app.powerbi.com/reportEmbed?reportId=30a825e3-341b-40a9-9466-ae3763719b5e&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVVTLUNFTlRSQUwtQS1QUklNQVJZLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0IiwiZW1iZWRGZWF0dXJlcyI6eyJtb2Rlcm5FbWJlZCI6dHJ1ZSwidXNhZ2VNZXRyaWNzVk5leHQiOnRydWV9fQ%3d%3d",//'<Embed Url>',
+  
+            accessToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvYzMwZmViZWUtNzU4Yy00ZGUxLWJhNTEtYzdmZjNhNWE3MGM2LyIsImlhdCI6MTY4OTcwMDY3OSwibmJmIjoxNjg5NzAwNjc5LCJleHAiOjE2ODk3MDU4NjksImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVFFBeS84VEFBQUFFTnM2d2VuTHBTaUlpRVhhZitOYW14SndGNXRYZ3BYU1lFUjdPRnRsQ1FGcjRjdXdzRHpZSkdVZkIrNE91bThuIiwiYW1yIjpbInB3ZCJdLCJhcHBpZCI6Ijg3MWMwMTBmLTVlNjEtNGZiMS04M2FjLTk4NjEwYTdlOTExMCIsImFwcGlkYWNyIjoiMCIsImlwYWRkciI6IjE3NS4xMTUuMjMzLjE3NiIsIm5hbWUiOiJEYW5pZWwgRmFybGV5Iiwib2lkIjoiMWE0YjM0MWEtNTlhOC00YjFiLTgzMjMtNzc2MzU2ZTNmNzc3IiwicHVpZCI6IjEwMDMyMDAwRkNFQzc1NkYiLCJyaCI6IjAuQVJNQTd1c1B3NHgxNFUyNlVjZl9PbHB3eGdrQUFBQUFBQUFBd0FBQUFBQUFBQURhQU1jLiIsInNjcCI6InVzZXJfaW1wZXJzb25hdGlvbiIsInN1YiI6IllZLVRQamF1a0JBU2Flc3JxdDUzS1R6NXloR25nVWlyQ2tiUlROalBCWUEiLCJ0aWQiOiJjMzBmZWJlZS03NThjLTRkZTEtYmE1MS1jN2ZmM2E1YTcwYzYiLCJ1bmlxdWVfbmFtZSI6IksxMjY1MEBvZmZpY2UzNjUuYmx1ZSIsInVwbiI6IksxMjY1MEBvZmZpY2UzNjUuYmx1ZSIsInV0aSI6IkNOeENXR19PVVVHVGhncXBYYWNDQUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbImI3OWZiZjRkLTNlZjktNDY4OS04MTQzLTc2YjE5NGU4NTUwOSJdfQ.mt2Fk9JoJr4uZTCnDDEuo978Xym4ysn6mc1gB0SPqBLRhpObdBPGNMe7dYHdSOjyOKJ8XClzH8tm4uvz8zB0hNW4qfRQwT2uhtQrdIb3WwTDXoduqdGKTPMvjdEWrLpLaNEmDV0SuoaMhoPeIW6GDzDJceYlhHSXZrv-OJpuoNU2L2ktqN60gx7NTnQkdrljt2W7oKs1prV0EbbQnaz5xPEKGrLTMZ8Ab4kjmaY7ivKZ9kEeQceE7H4HV3KDNKbDvh8SNZF3gRzMhdpv4y3NZ37fzireJhQHPqxVL3Gl8L_ZXxtNLUwyUjetsAs2Ktr9sqSl9R74D7oFNc3_Dw_KSg',  // '<Access Token>',
+          
+            tokenType: models.TokenType.Aad, // Use models.TokenType.Aad for SaaS embed
             settings: {
               panes: {
                 filters: {
                   expanded: false,
-                  visible: false
+                  visible: true
                 }
               },
               background: models.BackgroundType.Transparent,
@@ -37,10 +40,11 @@ function App() {
             ])
           }
 
-          cssClassName = { "reportClass" }
+          cssClassName = { "Embed-container" }
 
           getEmbeddedComponent = { (embeddedReport) => {
-            window.report = embeddedReport;
+            // window.report = embeddedReport;
+            window.Report = embeddedReport;
           }}
         />
       </header>
