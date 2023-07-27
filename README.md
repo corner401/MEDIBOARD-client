60ec1145f8aca0507029e1e635f055e9da81e71d
jimin-ni <jimin326ww@gmail.com>
2023-07-28 07:39:17 +0900

    [0728] 김지민 로그인 및 회원가입 페이지

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 김지민 파일 설명
     폴더명 : my-app
-    상태 : react 연결하기 위해서 생성함
-    power bi : ms 사이트에서 power bi 를 생성한 react 프로젝트에 넣기 위해서 group id와 reports id가 필요한데, 
-                현재 group id 를 알 수 없어 프로젝트 진도가 멈춤. 실습을 진행할 수 없음. power bi 유료 버전을 사용해야 이것이 나오는지도 모르겠음. 
+    상태 : react와 power bi 연결을 위해 생성
+    power bi : 
+        ms 사이트에서 power bi 를 생성한 react 프로젝트에 넣기 위해서 reports id를 이용하는 방법 만으로 진행. 
+        npm start 후 기본 포트 주소로 연결하면 확인 가능
 
-                * power bi 데이터 셋에 공백이나 빈 칸이 있으면 데이터를 생성하지 못함.
+    로그인 및 회원가입 화면 생성 완료
+        public 폴더 내 login.html, signin.html 파일, css, media 폴더 내 css 파일과 이미지 파일 첨부.
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { PowerBIEmbed } from 'powerbi-client-react';
 import { models } from 'powerbi-client';
@@ -8,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <PowerBIEmbed
           embedConfig = {{
             type: 'report',   // Supported types: report, dashboard, tile, visual, qna, paginated report and create
@@ -16,8 +14,9 @@ function App() {
             
             embedUrl: "https://app.powerbi.com/reportEmbed?reportId=30a825e3-341b-40a9-9466-ae3763719b5e&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVVTLUNFTlRSQUwtQS1QUklNQVJZLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0IiwiZW1iZWRGZWF0dXJlcyI6eyJtb2Rlcm5FbWJlZCI6dHJ1ZSwidXNhZ2VNZXRyaWNzVk5leHQiOnRydWV9fQ%3d%3d",//'<Embed Url>',
   
-            accessToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvYzMwZmViZWUtNzU4Yy00ZGUxLWJhNTEtYzdmZjNhNWE3MGM2LyIsImlhdCI6MTY4OTcwMDY3OSwibmJmIjoxNjg5NzAwNjc5LCJleHAiOjE2ODk3MDU4NjksImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVFFBeS84VEFBQUFFTnM2d2VuTHBTaUlpRVhhZitOYW14SndGNXRYZ3BYU1lFUjdPRnRsQ1FGcjRjdXdzRHpZSkdVZkIrNE91bThuIiwiYW1yIjpbInB3ZCJdLCJhcHBpZCI6Ijg3MWMwMTBmLTVlNjEtNGZiMS04M2FjLTk4NjEwYTdlOTExMCIsImFwcGlkYWNyIjoiMCIsImlwYWRkciI6IjE3NS4xMTUuMjMzLjE3NiIsIm5hbWUiOiJEYW5pZWwgRmFybGV5Iiwib2lkIjoiMWE0YjM0MWEtNTlhOC00YjFiLTgzMjMtNzc2MzU2ZTNmNzc3IiwicHVpZCI6IjEwMDMyMDAwRkNFQzc1NkYiLCJyaCI6IjAuQVJNQTd1c1B3NHgxNFUyNlVjZl9PbHB3eGdrQUFBQUFBQUFBd0FBQUFBQUFBQURhQU1jLiIsInNjcCI6InVzZXJfaW1wZXJzb25hdGlvbiIsInN1YiI6IllZLVRQamF1a0JBU2Flc3JxdDUzS1R6NXloR25nVWlyQ2tiUlROalBCWUEiLCJ0aWQiOiJjMzBmZWJlZS03NThjLTRkZTEtYmE1MS1jN2ZmM2E1YTcwYzYiLCJ1bmlxdWVfbmFtZSI6IksxMjY1MEBvZmZpY2UzNjUuYmx1ZSIsInVwbiI6IksxMjY1MEBvZmZpY2UzNjUuYmx1ZSIsInV0aSI6IkNOeENXR19PVVVHVGhncXBYYWNDQUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbImI3OWZiZjRkLTNlZjktNDY4OS04MTQzLTc2YjE5NGU4NTUwOSJdfQ.mt2Fk9JoJr4uZTCnDDEuo978Xym4ysn6mc1gB0SPqBLRhpObdBPGNMe7dYHdSOjyOKJ8XClzH8tm4uvz8zB0hNW4qfRQwT2uhtQrdIb3WwTDXoduqdGKTPMvjdEWrLpLaNEmDV0SuoaMhoPeIW6GDzDJceYlhHSXZrv-OJpuoNU2L2ktqN60gx7NTnQkdrljt2W7oKs1prV0EbbQnaz5xPEKGrLTMZ8Ab4kjmaY7ivKZ9kEeQceE7H4HV3KDNKbDvh8SNZF3gRzMhdpv4y3NZ37fzireJhQHPqxVL3Gl8L_ZXxtNLUwyUjetsAs2Ktr9sqSl9R74D7oFNc3_Dw_KSg',  // '<Access Token>',
-          
+            accessToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvYzMwZmViZWUtNzU4Yy00ZGUxLWJhNTEtYzdmZjNhNWE3MGM2LyIsImlhdCI6MTY4OTk0MDkwMywibmJmIjoxNjg5OTQwOTAzLCJleHAiOjE2ODk5NDY0NjcsImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVFFBeS84VEFBQUFXYmR2ZTlQamVCZllFczJTeERIamNGaFFuY01ONDAxRVV2NE5jdGhCM1dzeE5oNmx4bWIxcWJqaGFwSy9kcWVzIiwiYW1yIjpbInB3ZCJdLCJhcHBpZCI6Ijg3MWMwMTBmLTVlNjEtNGZiMS04M2FjLTk4NjEwYTdlOTExMCIsImFwcGlkYWNyIjoiMCIsImlwYWRkciI6IjE3NS4xMTUuMjMzLjE3NiIsIm5hbWUiOiJEYW5pZWwgRmFybGV5Iiwib2lkIjoiMWE0YjM0MWEtNTlhOC00YjFiLTgzMjMtNzc2MzU2ZTNmNzc3IiwicHVpZCI6IjEwMDMyMDAwRkNFQzc1NkYiLCJyaCI6IjAuQVJNQTd1c1B3NHgxNFUyNlVjZl9PbHB3eGdrQUFBQUFBQUFBd0FBQUFBQUFBQURhQU1jLiIsInNjcCI6InVzZXJfaW1wZXJzb25hdGlvbiIsInN1YiI6IllZLVRQamF1a0JBU2Flc3JxdDUzS1R6NXloR25nVWlyQ2tiUlROalBCWUEiLCJ0aWQiOiJjMzBmZWJlZS03NThjLTRkZTEtYmE1MS1jN2ZmM2E1YTcwYzYiLCJ1bmlxdWVfbmFtZSI6IksxMjY1MEBvZmZpY2UzNjUuYmx1ZSIsInVwbiI6IksxMjY1MEBvZmZpY2UzNjUuYmx1ZSIsInV0aSI6IlpkUVRuNWpjYkV1clI1aUJDSjZlQUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbImI3OWZiZjRkLTNlZjktNDY4OS04MTQzLTc2YjE5NGU4NTUwOSJdfQ.JHpe328bUYVY8Lz64RutimFCP0STk1LD-YNCOxlES1hm36F8_yp_eiI4R-PqsNvchaanwyVq0VpgLVRKoA-2N8z8nx1UwSbZ1XLG6ZG1YS-3FkUA1DaPmdNDHemhVOJVk7tEnxCo7DGAkqCdUOejjpXzZJfTS5htnxnSgtDcGTFyKlF2C0urNGIWEczkdiNSOxYJWxX5Ig4wuPzRVIc5dk81OiIujhcdX4XD6HDhh-97MGsPj_mUck8NUB5zexQnib7kUtPLLvo-WFX-fPZNpT1bYFemCjDp7uknSkGWhDbfB7hZ5skMSTvD9j04WRs8j3f-YfPtUD9HYUHU8qe7KA',
+          //* 1시간? 간격 갱신 필요 ㄷㄷ
+
             tokenType: models.TokenType.Aad, // Use models.TokenType.Aad for SaaS embed
             settings: {
               panes: {
@@ -47,6 +46,47 @@ function App() {
             window.Report = embeddedReport;
           }}
         />
+
+        <PowerBIEmbed
+          embedConfig = {{
+            type: 'report',   // Supported types: report, dashboard, tile, visual, qna, paginated report and create
+            id: "30a825e3-341b-40a9-9466-ae3763719b5e",  //* '<Report Id>',
+            
+            embedUrl: "https://app.powerbi.com/reportEmbed?reportId=30a825e3-341b-40a9-9466-ae3763719b5e&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVVTLUNFTlRSQUwtQS1QUklNQVJZLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0IiwiZW1iZWRGZWF0dXJlcyI6eyJtb2Rlcm5FbWJlZCI6dHJ1ZSwidXNhZ2VNZXRyaWNzVk5leHQiOnRydWV9fQ%3d%3d",
+  
+            accessToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvYzMwZmViZWUtNzU4Yy00ZGUxLWJhNTEtYzdmZjNhNWE3MGM2LyIsImlhdCI6MTY4OTk0MDkwMywibmJmIjoxNjg5OTQwOTAzLCJleHAiOjE2ODk5NDY0NjcsImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVFFBeS84VEFBQUFXYmR2ZTlQamVCZllFczJTeERIamNGaFFuY01ONDAxRVV2NE5jdGhCM1dzeE5oNmx4bWIxcWJqaGFwSy9kcWVzIiwiYW1yIjpbInB3ZCJdLCJhcHBpZCI6Ijg3MWMwMTBmLTVlNjEtNGZiMS04M2FjLTk4NjEwYTdlOTExMCIsImFwcGlkYWNyIjoiMCIsImlwYWRkciI6IjE3NS4xMTUuMjMzLjE3NiIsIm5hbWUiOiJEYW5pZWwgRmFybGV5Iiwib2lkIjoiMWE0YjM0MWEtNTlhOC00YjFiLTgzMjMtNzc2MzU2ZTNmNzc3IiwicHVpZCI6IjEwMDMyMDAwRkNFQzc1NkYiLCJyaCI6IjAuQVJNQTd1c1B3NHgxNFUyNlVjZl9PbHB3eGdrQUFBQUFBQUFBd0FBQUFBQUFBQURhQU1jLiIsInNjcCI6InVzZXJfaW1wZXJzb25hdGlvbiIsInN1YiI6IllZLVRQamF1a0JBU2Flc3JxdDUzS1R6NXloR25nVWlyQ2tiUlROalBCWUEiLCJ0aWQiOiJjMzBmZWJlZS03NThjLTRkZTEtYmE1MS1jN2ZmM2E1YTcwYzYiLCJ1bmlxdWVfbmFtZSI6IksxMjY1MEBvZmZpY2UzNjUuYmx1ZSIsInVwbiI6IksxMjY1MEBvZmZpY2UzNjUuYmx1ZSIsInV0aSI6IlpkUVRuNWpjYkV1clI1aUJDSjZlQUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbImI3OWZiZjRkLTNlZjktNDY4OS04MTQzLTc2YjE5NGU4NTUwOSJdfQ.JHpe328bUYVY8Lz64RutimFCP0STk1LD-YNCOxlES1hm36F8_yp_eiI4R-PqsNvchaanwyVq0VpgLVRKoA-2N8z8nx1UwSbZ1XLG6ZG1YS-3FkUA1DaPmdNDHemhVOJVk7tEnxCo7DGAkqCdUOejjpXzZJfTS5htnxnSgtDcGTFyKlF2C0urNGIWEczkdiNSOxYJWxX5Ig4wuPzRVIc5dk81OiIujhcdX4XD6HDhh-97MGsPj_mUck8NUB5zexQnib7kUtPLLvo-WFX-fPZNpT1bYFemCjDp7uknSkGWhDbfB7hZ5skMSTvD9j04WRs8j3f-YfPtUD9HYUHU8qe7KA',
+          //* 1시간? 간격 갱신 필요 ㄷㄷ
+
+            tokenType: models.TokenType.Aad, // Use models.TokenType.Aad for SaaS embed
+            settings: {
+              panes: {
+                filters: {
+                  expanded: false,
+                  visible: true
+                }
+              },
+              background: models.BackgroundType.Transparent,
+            }
+          }}
+
+          eventHandlers = {
+            new Map([
+              ['loaded', function () {console.log('Report loaded');}],
+              ['rendered', function () {console.log('Report rendered');}],
+              ['error', function (event) {console.log(event.detail);}],
+              ['visualClicked', () => console.log('visual clicked')],
+              ['pageChanged', (event) => console.log(event)],
+            ])
+          }
+
+          cssClassName = { "Embed-container" }
+
+          getEmbeddedComponent = { (embeddedReport) => {
+            // window.report = embeddedReport;
+            window.Report = embeddedReport;
+          }}
+        />
+
       </header>
     </div>
   );
