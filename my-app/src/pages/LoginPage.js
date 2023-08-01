cf1e098ac2d4d626f22909e6e6e9d2a20f228d31
unknown <lerle144@gmail.com>
2023-08-02 00:05:35 +0900

    회원가입, 로그인 페이지

--- my-app/src/pages/LoginPage.js
+++ my-app/src/pages/LoginPage.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import styles from '../css/login.module.css';
 
 const LoginPage = () => {
 
   return (
-    <div>
-      로그인 페이지입니다.
+    <div className={styles.login_stuff}>
+        <div className={styles.overlay}></div>   
+        <img src="./media/logo.png" alt="logo" id={styles.logo_img} /> 
+        <div id={styles.signup_stuff}>            
+            <h1 id={styles.service_name}> 서비스 명 </h1>  
+            <h2 id={styles.login}>Login</h2>
+            <div id={styles.box}>Email</div> 
+            <div id={styles.box}>Password</div> 
+            <a id={styles.sign_up_page} href="./signin.html">Sign Up</a>
+        </div>
+        <div id={styles.box_button}>Login </div> 
     </div>
   );
 };
--- my-app/src/pages/SignupPage.js
+++ my-app/src/pages/SignupPage.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import styles from '../css/signup.module.css';
 
 const SignupPage = () => {
 
   return (
-    <div>
-      회원가입 페이지입니다.
+    <div className={styles.login_stuff}>
+    <div className={styles.overlay}></div>   {/*  반투명 배경 블록 */}
+    <img src="./media/logo.png" alt="logo" id={styles.logo_img} />  {/* 로고 이미지 */}
+    <div id={styles.signup_stuff}>            
+        <h1 id={styles.service_name}> 서비스 명 </h1>  {/* 서비스 명 텍스트 */}
+        <h2 id={styles.sign_up}>Sign Up</h2>
+        <div id={styles.box}>Email</div> 
+        <div id={styles.box}>User Name</div> 
+        <div id={styles.box}>Password</div> 
+        <div id={styles.box}>Rewrite Password </div> 
+    </div>
+    <div id={styles.job_check}>
+        <img src="./media/Entrepreneur_check.png" alt="사업가" id="job_1" /> {/* 로고 이미지 */}
+        <img src="./media/Medical_staff_check.png" alt="의료진" id="job_2" /> {/* 로고 이미지 */}
+        <img src="./media/Patient_check.png" alt="환자" id="job_3" /> {/* 로고 이미지 */}
+        <img src="./media/Other_check.png" alt="기타" id="job_4" /> {/* 로고 이미지 */}
+    </div>
+    <div id={styles.box_button}>Sign Up </div> 
     </div>
   );
 };
