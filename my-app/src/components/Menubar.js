b25b735cdfc159ccc54bae6a4ef3a160e604505c
unknown <lerle144@gmail.com>
2023-08-04 04:52:40 +0900

    HashTag, Menubar 스타일 수정

--- my-app/src/components/Menubar.js
+++ my-app/src/components/Menubar.js
@@ -7,33 +7,33 @@ const Menubar = (props) => {
     return (
       <>
       <div className={styles.container}>
-        <Link className={styles.menu} to={'/'}>
+        <Link className={styles.logo} to={'/'}>
           <img className={styles.icon} src={require("../media/logo.png")} alt={`logo`} />
         </Link>
-      
+        <div className={styles.blank}/>
         <Link className={styles.menu} to={'/'}>
           <img className={styles.icon} src={require("../media/home_icon.png")} alt={`home`} />
-          <div>서비스 소개</div>
+          <div className={styles.menutext}>서비스 소개</div>
         </Link>
 
-        <Link to={'/dashboard'}>
+        <Link className={styles.menu} to={'/dashboard'}>
           <img className={styles.icon} src={require("../media/dashboard_icon.png")} alt={`dashboard`} />
-          <div>커스텀 대시보드</div>
+          <div className={styles.menutext}>커스텀 대시보드</div>
         </Link>
 
-        <Link to={'/chart'}>
+        <Link className={styles.menu} to={'/chart'}>
           <img className={styles.icon} src={require("../media/chart_icon.png")} alt={`chart`} />
-          <div>통계 자료 열람</div>
+          <div className={styles.menutext}>통계 자료 열람</div>
         </Link>
 
-        <Link to={'/news'}>
+        <Link className={styles.menu} to={'/news'}>
           <img className={styles.icon} src={require("../media/news_icon.png")} alt={`news`} />
-          <div>뉴스</div>
+          <div className={styles.menutext}>뉴스</div>
         </Link>
 
-        <Link to={'/rank'}>
+        <Link className={styles.menu} to={'/rank'}>
           <img className={styles.icon} src={require("../media/rank_icon.png")} alt={`rank`} />
-          <div>랭킹</div>
+          <div className={styles.menutext}>랭킹</div>
         </Link>
       </div>
       </>
