0fa515b02fc5fb8f787a975575b9708fc99808e0
jimin-ni <jimin326ww@gmail.com>
2023-08-05 01:19:01 +0900

    [0805] 예비 커밋

--- my-app/src/pages/ChartListPage.js
+++ my-app/src/pages/ChartListPage.js
@@ -8,7 +8,7 @@ const ChartListPage = () => {
 
   return (
     <div>
-      <img src="../media/nav.png" alt="좌측 나브 바" /> 
+      {/* <img src="../media/nav.png" alt="좌측 나브 바" />  */}
       
       <div> {/* 상단 나브 바*/}
         <img src="../media/chartlistpage_nav.png" alt="상단 나브 바" /> 
@@ -88,11 +88,12 @@ const ChartListPage = () => {
           <PowerBIEmbed
               embedConfig = {{
                 type: 'report',   // Supported types: report, dashboard, tile, visual, qna, paginated report and create
-                id: "99fc4531-73f3-4bda-b8f9-ef2d9948d577",  //* '<Report Id>',
+                id: "1fa9d1ca-2c6a-4a1e-a7eb-2ea4c6914a1f",  //* '<Report Id>',
                 
-                embedUrl: "https://app.powerbi.com/reportEmbed?reportId=30a825e3-341b-40a9-9466-ae3763719b5e&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVVTLUNFTlRSQUwtQS1QUklNQVJZLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0IiwiZW1iZWRGZWF0dXJlcyI6eyJtb2Rlcm5FbWJlZCI6dHJ1ZSwidXNhZ2VNZXRyaWNzVk5leHQiOnRydWV9fQ%3d%3d",//'<Embed Url>',
-                accessToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvYzMwZmViZWUtNzU4Yy00ZGUxLWJhNTEtYzdmZjNhNWE3MGM2LyIsImlhdCI6MTY5MDk4MDQ0NywibmJmIjoxNjkwOTgwNDQ3LCJleHAiOjE2OTA5ODU3MjAsImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVFFBeS84VUFBQUFLeEsrOGdKck5yazdnQWw3VCt4SG1pRU5HeDA4a29IdlJ5L3AyTFI5dWI1TUpvRCtLSTRUNVZkME1rQ21tb3VQIiwiYW1yIjpbInB3ZCJdLCJhcHBpZCI6Ijg3MWMwMTBmLTVlNjEtNGZiMS04M2FjLTk4NjEwYTdlOTExMCIsImFwcGlkYWNyIjoiMCIsImlwYWRkciI6IjE3NS4xMTUuMjMzLjE3NiIsIm5hbWUiOiJEYW5pZWwgRmFybGV5Iiwib2lkIjoiMWE0YjM0MWEtNTlhOC00YjFiLTgzMjMtNzc2MzU2ZTNmNzc3IiwicHVpZCI6IjEwMDMyMDAwRkNFQzc1NkYiLCJyaCI6IjAuQVJNQTd1c1B3NHgxNFUyNlVjZl9PbHB3eGdrQUFBQUFBQUFBd0FBQUFBQUFBQURhQU1jLiIsInNjcCI6InVzZXJfaW1wZXJzb25hdGlvbiIsInNpZ25pbl9zdGF0ZSI6WyJrbXNpIl0sInN1YiI6IllZLVRQamF1a0JBU2Flc3JxdDUzS1R6NXloR25nVWlyQ2tiUlROalBCWUEiLCJ0aWQiOiJjMzBmZWJlZS03NThjLTRkZTEtYmE1MS1jN2ZmM2E1YTcwYzYiLCJ1bmlxdWVfbmFtZSI6IksxMjY1MEBvZmZpY2UzNjUuYmx1ZSIsInVwbiI6IksxMjY1MEBvZmZpY2UzNjUuYmx1ZSIsInV0aSI6IkRkVWJkNFRvVGtPQTBoTERMZmhVQUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbImI3OWZiZjRkLTNlZjktNDY4OS04MTQzLTc2YjE5NGU4NTUwOSJdfQ.Voak5PXKWuv6IqtjbsTZVC099sAR8z8LXmAWRgmnyCktmoGwZXxLmIkweU4o7DRRs8I2cMU-x6ey-Q16bsswEemUM14TNAWNENnGKmHVvgo_4XajMJJ1JrFPU-HM9ji-EX3F37aZrVw9re0-FnAik6dn8EDtqjhUWb7nw3Anf81DEfR09pH_4rwcN1-zQimDlcR5IhOFcbX2a6uUUJo7PWBNsdGZ26-fJWk0JfCIqBPNz3IbjSoYeK704zFzcG4AhEUesw0T7ZU6euweZdbrjVY_GQIjcD3OIE3SA4a0yiDQkfOFbYEy2UslPn9G-dbQ1oZaSXEjM1UVisqqYAhGDA',
-              //* 1시간? 간격 갱신 필요 ㄷㄷ
+                // 아래 주소는 다른 계정의 보고서
+                embedUrl: "https://app.powerbi.com/reportEmbed?reportId=1fa9d1ca-2c6a-4a1e-a7eb-2ea4c6914a1f&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLUVBU1QtQVNJQS1BLVBSSU1BUlktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQiLCJlbWJlZEZlYXR1cmVzIjp7Im1vZGVybkVtYmVkIjp0cnVlLCJ1c2FnZU1ldHJpY3NWTmV4dCI6dHJ1ZX19",
+                accessToken:"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvZjljMTAwMDMtNDU0My00MDc5LTg5Y2UtZTU3OGM5OTA0MTljLyIsImlhdCI6MTY5MTA2NDg5MiwibmJmIjoxNjkxMDY0ODkyLCJleHAiOjE2OTEwNzA1NTUsImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVFFBeS84VUFBQUFsY1Y3cU4xSTFjMzd6YTBvaVYzUTNkM1BDakFPcDFkV2NLWDBqYm14L2lxNXlGM1FxUFBmTDlTNnZiNlJVTW4rIiwiYW1yIjpbInB3ZCIsInJzYSJdLCJhcHBpZCI6Ijg3MWMwMTBmLTVlNjEtNGZiMS04M2FjLTk4NjEwYTdlOTExMCIsImFwcGlkYWNyIjoiMCIsImRldmljZWlkIjoiMjA0MmZkMDUtYjMyZC00ZjBlLWIwZWUtZGQwN2E2NDQ3OWE3IiwiaXBhZGRyIjoiMTc1LjE5Mi41LjE5MyIsIm5hbWUiOiJneHEiLCJvaWQiOiI1YmY0NDM1Yi04MmY0LTQ3OGMtOWM3MC1hNWQ4YTY3NDU3YmMiLCJwdWlkIjoiMTAwMzIwMDA3NjVDQTBERiIsInJoIjoiMC5BU29BQXdEQi1VTkZlVUNKenVWNHlaQkJuQWtBQUFBQUFBQUF3QUFBQUFBQUFBQXFBTk0uIiwic2NwIjoidXNlcl9pbXBlcnNvbmF0aW9uIiwic2lnbmluX3N0YXRlIjpbImttc2kiXSwic3ViIjoieERIYmVMX0pIZFZPZURNYmNoZDdodUZhUVdPT210VExBMWEteGw3Z1hLOCIsInRpZCI6ImY5YzEwMDAzLTQ1NDMtNDA3OS04OWNlLWU1NzhjOTkwNDE5YyIsInVuaXF1ZV9uYW1lIjoiY3FmeHFAa3FxbGMub25taWNyb3NvZnQuY29tIiwidXBuIjoiY3FmeHFAa3FxbGMub25taWNyb3NvZnQuY29tIiwidXRpIjoiaWlsWVkzc0hYVW1sRUtEb0VoUWxBQSIsInZlciI6IjEuMCIsIndpZHMiOlsiYjc5ZmJmNGQtM2VmOS00Njg5LTgxNDMtNzZiMTk0ZTg1NTA5Il19.gQcUuT3mRljdFnfHz4Q1ipDyDbIWNEX9cIUE6q0SEn7TEwmrfRwGww2vVrGbT1_Hb_9U5Re3f1PqHCJFGadpnROTNgpZ6TEn1zOjz5-ukH1Rc5GG_tnakHNGIr_7j47Hhhtr2UTe9uCBOnmeTrlG4lB3bFzO8c0Rj3zFwte5QJrAvmpwKsqg1P3QNE89nEYxbOLO-41LLkj9uAdpSKqlpP2y9omkY7i4YhtN-3qcqgLOU8XzNj68XUJxHrOnPyrrfjtKiYh1FCp9F6Rd-5yzxgUitpjCWEDzeKhVL9MQMjqZHLqBpFBismCi4rjfkA5bmWfrAnCiWi8dSadTDYrvMg",
+ //* 1시간? 간격 갱신 필요 ㄷㄷ
 
                 tokenType: models.TokenType.Aad, // Use models.TokenType.Aad for SaaS embed
                 settings: {
