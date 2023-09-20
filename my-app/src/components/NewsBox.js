6e001accf766333d10a56baf9bd54147ac7aef20
ose0221 <ose0221@naver.com>
2023-09-20 22:48:21 +0900

    NewsBox 컴포넌트 크기 조절

--- my-app/src/components/NewsBox.js
+++ my-app/src/components/NewsBox.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import styles from '../css/newsBox.module.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import styles from "../css/newsBox.module.css";
+import { Link } from "react-router-dom";
 
 const NewsBox = (props) => {
     const data = props.data;
 
     return (
         <Link className={styles.main_stuff} to={data.link}>
-            <h2 id={styles.title}>{data.title}</h2>       {/* 제목 title */}
+            <h2 id={styles.title}>{data.title}</h2> {/* 제목 title */}
             <div id={styles.content}>
                 <p id={styles.content_text}>{data.content}</p>
             </div>
             <div className={styles.discribe_box}>
-                <p id={styles.author}>{data.writer}</p>기자       {/* 날짜 date */}
-                <p id={styles.date}>{data.updateDate.substr(0,10)}</p>           {/* 뉴스 출처 author */}
+                <p id={styles.author}>{data.writer}</p>기자 {/* 날짜 date */}
+                <p id={styles.date}>{data.updateDate.substr(0, 10)}</p> {/* 뉴스 출처 author */}
             </div>
         </Link>
     );
